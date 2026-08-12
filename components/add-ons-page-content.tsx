573f4fbe48a67f89619316aaa93f36eb77b0cf83
Tgmedia01 <126424437+Tgmedia01@users.noreply.github.com>
2026-08-12 13:25:26 +0100

    Add CTA text 'Find Out More' to add-ons content

--- components/add-ons-page-content.tsx
+++ components/add-ons-page-content.tsx
@@ -112,7 +112,7 @@ const addOns = [
       { src: "/images/DSC00641-2.jpg", caption: "Couple on the floor" },
       { src: "/images/s_wphotography_ashleytony_1393.jpg", caption: "Enhanced Tube Lighting" },
     ],
-  
+    cta: "Find Out More",
   },
 ]
 
